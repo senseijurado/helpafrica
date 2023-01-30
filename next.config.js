316b9ebc7eae507643e26504c6186f9c73323f1c
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-01-30 19:00:08 +0100

    feat: edited files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '.',
+  compress: true,
+  cleanDistDir: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+    dirs: ['./src/**/*.{tsx,ts,jsx,js}',]
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['']
+  }
 }
 
 module.exports = nextConfig
