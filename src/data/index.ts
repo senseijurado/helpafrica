c73fe2c75c5aea890147632b85aeeb1274de68b5
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-01-31 13:47:18 +0100

    fix: Add prettier-tailwind plugin

---
+++ src/data/index.ts
@@ -0,0 +1,17 @@
+
+
+export interface QuestionProps
+{
+    id: number;
+    question?: string;
+    answer?: string;
+}
+
+export let FAQS: QuestionProps[] = [
+    {
+        id: 1,
+        question: '',
+        answer: ''
+    }
+]
+
