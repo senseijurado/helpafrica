a7e25b5d39ba13b5a0aeac33ca8983e5e944d964
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-02-12 22:32:46 +0100

    fix: import open-props styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '@/styles/globals.css'
-import 'https://unpkg.com/open-props'
 import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps, router }: AppProps) {
