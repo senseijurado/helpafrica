2092c0cd506f17cfeed164a545a8971b84386308
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-02-13 14:32:56 +0100

    feat: update color palette and install picocss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,18 @@ export const Document = () => {
 		<>
 			<Html lang='en' className='dark dark:bg-gray-500' data-theme='darktheme'>
 				<Head>
-					{/* Google Fonts */}
+					{/* GOOGLE FONTS: Inter Sans Serif |  */}
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
 					<link rel='preconnect' href='https://fonts.gstatic.com' />
 					<link
-						href='https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;500;700&=Inter:wght@300;500;700&display=swap'
+						href='https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;500;700;900&family=Inter:wght@100;300;500;700;900&display=swap'
 						rel='stylesheet'
+					/>
+
+					{/* LOAD PICOCSS */}
+					<link
+						rel='stylesheet'
+						href='https://unpkg.com/@picocss/pico@1.*/css/pico.min.css'
 					/>
 				</Head>
 				<body className='bg-primary-400  min-h-screen'>
